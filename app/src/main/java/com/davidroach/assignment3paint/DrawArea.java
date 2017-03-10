073d3fafb17d099292b703e82800253a1915f2e3
droach-dev <mrroachhimself@gmail.com>
2017-03-09 18:11:30 -0600

    Have a simple draw Canvas.  Need to work on features next.

--- app/src/main/java/com/davidroach/assignment3paint/DrawArea.java
+++ app/src/main/java/com/davidroach/assignment3paint/DrawArea.java
@@ -2,10 +2,13 @@ package com.davidroach.assignment3paint;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 import android.graphics.Paint;
 import android.graphics.Color;
+import android.graphics.Path;
 import android.graphics.Canvas;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
 import android.util.Log;
 
@@ -19,8 +22,14 @@ public class DrawArea extends View {
     //Brush atrributes set in this class for now.
     //May move to main  class if the need arises.
 
+
+
     private int currentColor;
-    private Paint paint;
+    private Paint paintBrush;
+    private Canvas appCanvas;
+    private Paint canvasPaint;
+    private Path linePath;
+    private Bitmap appBitmap;
 
 
 
@@ -47,7 +56,8 @@ public class DrawArea extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        canvas.drawPaint(paint);
+        canvas.drawBitmap(appBitmap, 0, 0, canvasPaint);
+        canvas.drawPath(linePath, paintBrush);
     }
 
 
@@ -55,26 +65,76 @@ public class DrawArea extends View {
     //sets color variable to be used in brushes and fonts.
     private void setCurrentColor(int colorIn){
         currentColor = colorIn;
+        //paintBrush.setColor(colorIn);
 
     }
 
 
     //
     public void setup(AttributeSet attrs){
-        currentColor = Color.BLACK;
-        paint = new Paint();
+        setCurrentColor(Color.BLACK);
 
+        //
+        appCanvas = new Canvas();
+
+        linePath = new Path();
+
+        //Set paint brush initial settings
+        paintBrush = new Paint();
+        paintBrush.setAntiAlias(true);
+        paintBrush.setStrokeWidth(50);
+        paintBrush.setStrokeJoin(Paint.Join.ROUND);
+        paintBrush.setStrokeCap(Paint.Cap.ROUND);
+        paintBrush.setStyle(Paint.Style.STROKE);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
     }
+
+
 
     //fill background with color
     public void changeBackgroundColor(int colorIn){
         currentColor = colorIn;
-        paint.setColor(currentColor);
+        paintBrush.setColor(currentColor);
         Log.i("CHANGED", "Filled Canvas with color.");
         invalidate();
 
     }
 
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        float xCord = event.getX();
+        float yCord = event.getY();
+
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                linePath.moveTo(xCord, yCord);
+                break;
+            case MotionEvent.ACTION_UP:
+                appCanvas.drawPath(linePath, paintBrush);
+                linePath.reset();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                linePath.lineTo(xCord, yCord);
+                break;
+            default:
+                return false;
+        }
+
+        //redraw Canvas
+        invalidate();
+        return true;
+    }
+
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        appBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        appCanvas = new Canvas(appBitmap);
+
+    }
 
 }
